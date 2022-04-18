7177440a531b66dfd6d8c66634145d1262c92f1b
Charanvir <charanvir123@gmail.com>
2022-04-17 20:12:09 -0400

    tested and created the following functions: getAttackValue, addPotion, usePotion

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -61,3 +61,32 @@ test("subtracts from player's health", () => {
 
     expect(player.health).toBe(0)
 })
+
+test("gets players attack value", () => {
+    const player = new Player("Charanvir")
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5)
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15)
+})
+
+test("adds a potion to the inventory", () => {
+    const player = new Player("Charanvir")
+    const oldCount = player.inventory.length
+
+    player.addPotion(new Potion())
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount)
+})
+
+test("uses a potion from inventory", () => {
+    const player = new Player("Charanvir")
+
+    player.inventory = [new Potion(), new Potion(), new Potion()]
+
+    const oldCount = player.inventory.length
+
+    player.usePotion(1)
+
+    expect(player.inventory.length).toBeLessThan(oldCount)
+})
